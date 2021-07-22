b952617132b3a64766dfeeb0ce66321ef83cd2b2
Angel Francis <angelfrancis1111@gmail.com>
2021-07-22 11:03:46 +0530

    domain

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 // app.use(cors());
 app.use(express.static(__dirname + "/"));
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header("Access-Control-Allow-Origin", "https://objective-swanson-18e779.netlify.app/");
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
