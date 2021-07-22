fd27d48e1a0fb1665bbd1228c594b19c88a278ca
Angel Francis <angelfrancis1111@gmail.com>
2021-07-22 11:12:43 +0530

    domain

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ const Signup = require("./controllers/Signup.js");
 const Login = require("./controllers/Login.js");
 const Upload = require("./controllers/Upload.js");
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors());
 app.use(express.static(__dirname + "/"));
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 	next();
 });
 app.post("/upload", Upload.upload.single("imgUploader"), (req, res, next) => {
